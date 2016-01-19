390dd1048ab016fd8e89fbd0027ac35e364cb4a0
Irenej Marc <irenej.marc@nuclei.co>
2016-01-19 21:33:15 +0100

    Added a query and querybuilder class

---
+++ source/dpq/query.d
@@ -0,0 +1,82 @@
+module dpq.query;
+
+import dpq.connection;
+import dpq.value;
+import dpq.exception;
+import dpq.value;
+import dpq.result;
+
+struct Query
+{
+	private string _command;
+	private Value[] _params;
+	private Connection* _connection;
+
+	this(ref Connection connection, string command)
+	{
+		_connection = &connection;
+		_command = command;
+	}
+
+	this(string command)
+	{
+		if (_dpqLastConnection == null)
+			throw new DPQException("Query: No established connection was found and none was provided.");
+
+		_connection = _dpqLastConnection;
+		_command = command;
+	}
+
+	@property void connection(ref Connection conn)
+	{
+		_connection = &conn;
+	}
+
+	@property string command()
+	{
+		return _command;
+	}
+	
+	@property void command(string c)
+	{
+		_command = c;
+	}
+
+	void addParam(T)(T val)
+	{
+		_params ~= Value(val);
+	}
+
+	ref Query opBinary(string op, T)(T val)
+			if (op == "<<")
+	{
+		addParam(val);
+		return this;
+	}
+
+	void opAssign(string str)
+	{
+		command = str;
+	}
+
+	@property Result run()
+	{
+		import std.datetime;
+
+		StopWatch sw;
+		sw.start();
+		auto r = _connection.execParams(_command, _params);
+		sw.stop();
+
+		r.time = sw.peek();
+		return r;
+	}
+
+	Result run(T...)(T params)
+	{
+		foreach (p; params)
+			addParam(p);
+
+		return run();
+	}
+}
---
+++ source/dpq/querybuilder.d
@@ -0,0 +1,113 @@
+module dpq.querybuilder;
+
+import dpq.value;
+import dpq.query;
+
+import std.typecons;
+import std.string;
+
+enum Order : string
+{
+	asc = "ASC",
+	desc = "DESC"
+};
+
+struct QueryBuilder
+{
+	// SELECT 'a', 'b' FROM table_name WHERE id = 1 ORDER BY id DESC LIMIT 10 OFFSET 5 
+
+	private
+	{
+		string[] _columns;
+		string _from;
+		string _where;
+		string[] _orderBy;
+		Order[] _orders;
+		int _limit = -1;
+		Nullable!int _offset;
+		Value[string] _params;
+
+		int _paramIndex = 1;
+	}
+
+	void opIndexAssign(T)(T val, string key)
+	{
+		_params[key] = val;
+	}
+
+
+	ref QueryBuilder from(string from)
+	{
+		_from = from;
+		return this;
+	}
+
+	ref QueryBuilder where()(string filter)
+	{
+		_where = filter;
+		return this;
+	}
+
+	ref QueryBuilder order(string col, Order order)
+	{
+		_orderBy ~= col;
+		_orders ~= order;
+		return this;
+	}
+	
+	ref QueryBuilder limit(int limit)
+	{
+		_limit = limit;
+		return this;
+	}
+
+	ref QueryBuilder offset(int offset)
+	{
+		_offset = offset;
+		return this;
+	}
+
+	@property string command()
+	{
+		string cols;
+		if (_columns.length == 0)
+			cols = "*";
+		else
+			cols = _columns.join("\",\"");
+
+		string str = "SELECT %s FROM \"%s\"".format(cols, _from);
+
+		if (_where.length > 0)
+			str ~= " WHERE " ~ _where;
+
+		if (_orderBy.length > 0)
+		{
+			str ~= " ORDER BY ";
+			for (int i = 0; i < _orderBy.length; ++i)
+				str ~= "\"" ~ _orderBy[i] ~ "\" " ~ _orders[i] ~ ", ";
+			str = str[0 .. $ - 2];
+		}
+
+		if (_limit != -1)
+			str ~= " LIMIT %d".format(_limit);
+
+		if (!_offset.isNull)
+			str ~= " OFFSET %d".format(_offset);
+
+		foreach (param, val; _params)
+			str = str.replace("{" ~ param ~ "}", "$%d".format(_paramIndex++));
+
+		return str;
+	}
+
+	@property Query query()
+	{
+		return Query(command());
+	}
+	
+}
+
+void param(alias P)(QueryBuilder b)
+{
+	b[P.stringof] = P;
+}
