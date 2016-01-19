233f4ba4c2444c07ef70390d8e23a2eaf2399229
Irenej Marc <irenej.marc@nuclei.co>
2016-01-19 21:34:22 +0100

    updates the Value struct

--- source/dpq/value.d
+++ source/dpq/value.d
@@ -1,18 +1,33 @@
 module dpq.value;
 
+import dpq.result;
+
 import derelict.pq.pq;
 
 import std.algorithm : map;
 import std.variant;
 import std.array;
 import std.conv : to;
+import std.typecons : Nullable;
 
-struct SQLValue
+struct Value
 {
 	ubyte[] _valueBytes;
 	private int _size;
 
 	this(T)(T val)
+	{
+		this.opAssign(val);
+	}
+
+	this(T)(T* val, int len)
+	{
+		_size = len;
+		for (int i = 0; i < len; ++i)
+			_valueBytes ~= val[i];
+	}
+
+	void opAssign(T)(T val)
 	{
 		import std.bitmanip;
 
@@ -20,10 +35,9 @@ struct SQLValue
 
 		_valueBytes = new ubyte[_size];
 		write(_valueBytes, val, 0);
-		std.stdio.writeln("ByteArray: ", _valueBytes);
 	}
 
-	this(string val)
+	void opAssign(string val)
 	{
 		_valueBytes = cast(ubyte[])val.dup;
 		_size = _valueBytes.length.to!int;
@@ -44,24 +58,32 @@ struct SQLValue
 	{
 		return _valueBytes.ptr;
 	}
+
+	Nullable!T as(T)()
+	{
+		import std.bitmanip;
+		const(ubyte)[] data = _valueBytes[0 .. _size];
+
+		return fromBytes!T(data);
+	}
 }
 
-Oid[] paramTypes(SQLValue[] values)
+Oid[] paramTypes(Value[] values)
 {
 	return array(values.map!(v => v.type));
 }
 
-int[] paramLengths(SQLValue[] values)
+int[] paramLengths(Value[] values)
 {
 	return array(values.map!(v => v.size));
 }
 
-int[] paramFormats(SQLValue[] values)
+int[] paramFormats(Value[] values)
 {
 	return array(values.map!(v => 1));
 }
 
-const(ubyte)*[] paramValues(SQLValue[] values)
+const(ubyte)*[] paramValues(Value[] values)
 {
 	return array(values.map!(v => v.valuePointer));
 }
