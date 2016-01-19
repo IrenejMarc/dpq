d1c2e8afdd78e4fa8392b9cd19d1bc46cdc14784
Irenej Marc <irenej.marc@nuclei.co>
2016-01-19 21:30:13 +0100

    Usage examples and testing of the new features

--- source/app.d
+++ source/app.d
@@ -1,13 +1,15 @@
 import std.stdio;
+import std.typecons;
 
 import dpq.connection;
+import dpq.querybuilder;
+import dpq.query;
 
-import std.stdio;
 
 void main()
 {
 	writeln("Connecting");
-	auto conn = SQLConnection("host=10.2.0.2 dbname=testdb user=testuser password='VerySecureTestPassword'");
+	auto conn = Connection("host=10.2.0.2 dbname=testdb user=testuser password='VerySecureTestPassword'");
 	writefln("Error message from connection: \"%s\"", conn.errorMessage);
 	writeln("Connected");
 	writefln("Database name is: %s.", conn.db);
@@ -21,8 +23,39 @@ void main()
 	writefln("Query result rows: %d.", res.rows);
 	writefln("Query result columns: %d.", res.columns);
 
-	writefln("result.get(0, 0) is: %d", res.get!int(0, 0));
-	writefln("result.get(0, 2) is: %s", res.get!string(0, 1));
+	writefln("result.get(0, 0) is: %d", res.get(0, 0).as!int);
+	writefln("result.get(0, 2) is: %s", res.get(0, 1).as!string);
 
+	int id = 1;
 
+	QueryBuilder b;
+	b.from("users")
+		.where("id = {id}")
+		.order("id", Order.desc)
+		.order("asd", Order.asc)
+		.limit(1)
+		.offset(5);
+
+	b.param!id;
+
+	writeln(b.command);
+
+	auto q = Query("SELECT 1, 'asd', $1::int, $2::text");
+	q << 123 << "asd";
+	q.command = "SELECT 0.000000000000001::float, $1::int, $2::text";
+
+	writeln("Command: ", q.command);
+	auto r = q.run();
+
+	auto val = r[0][0];
+	writeln("Result: ", val.as!double);
+
+	Query("CREATE TABLE IF NOT EXISTS test (id SERIAL, txt TEXT)").run();
+
+	q = Query("SELECT id, txt FROM test WHERE id = $1 OR id = $2");
+	r = q.run(4, 1);
+	foreach (row; r)
+		writeln("row['txt'] is: ", row["txt"].as!string);
+
+	writeln("usecs taken for query: ", r.time.usecs);
 }
