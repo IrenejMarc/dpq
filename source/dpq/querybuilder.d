c3f2c0ff792389d756cb6f8e68f7f55553cb86ea
Irenej Marc <irenej.marc@nuclei.co>
2016-01-28 02:16:37 +0100

    Added support for UPDATE queries to QueryBuilder

--- source/dpq/querybuilder.d
+++ source/dpq/querybuilder.d
@@ -14,6 +14,13 @@ enum Order : string
 	desc = "DESC"
 };
 
+private enum QueryType
+{
+	select = "SELECT",
+	update = "UPDATE",
+	insert = "INSERT"
+}
+
 struct QueryBuilder
 {
 	// SELECT 'a', 'b' FROM table_name WHERE id = 1 ORDER BY id DESC LIMIT 10 OFFSET 5 
@@ -21,16 +28,25 @@ struct QueryBuilder
 	private
 	{
 		string[] _columns;
-		string _from;
-		string _where;
+		string _table;
+		string _filter;
+
 		string[] _orderBy;
 		Order[] _orders;
+
 		int _limit = -1;
+
 		Nullable!int _offset;
 		Value[string] _params;
+		Value[] _indexParams;
 
-		int _paramIndex = 1;
+		// UPDATE's SET
+		string[] _set;
+
+		int _paramIndex = 0;
+		QueryType _type;
 		Connection* _connection;
+
 	}
 
 	this(ref Connection connection)
@@ -43,26 +59,38 @@ struct QueryBuilder
 		_params[key] = val;
 	}
 
+	// SELECT methods
 	ref QueryBuilder select(string[] cols...)
 	{
 		_columns = cols;
+		_type = QueryType.select;
 		return this;
 	}
 
 	ref QueryBuilder from(string from)
 	{
-		_from = from;
+		assert(_type == QueryType.select, "QueryBuilder.from() can only be used for SELECT queries.");
+		_table = from;
 		return this;
 	}
 
-	ref QueryBuilder where()(string filter)
+	ref QueryBuilder where(string filter)
 	{
-		_where = filter;
+		_filter = filter;
+		return this;
+	}
+
+	ref QueryBuilder where(T)(string col, T val)
+	{
+		addParam(val);
+		_filter = "\"%s\" = $%d".format(col, _paramIndex);
+
 		return this;
 	}
 
 	ref QueryBuilder order(string col, Order order)
 	{
+		assert(_type == QueryType.select, "QueryBuilder.order() can only be used for SELECT queries.");
 		_orderBy ~= col;
 		_orders ~= order;
 		return this;
@@ -70,17 +98,63 @@ struct QueryBuilder
 	
 	ref QueryBuilder limit(int limit)
 	{
+		assert(_type == QueryType.select, "QueryBuilder.limit() can only be used for SELECT queries.");
+
 		_limit = limit;
 		return this;
 	}
 
 	ref QueryBuilder offset(int offset)
 	{
+		assert(_type == QueryType.select, "QueryBuilder.offset() can only be used for SELECT queries.");
 		_offset = offset;
 		return this;
 	}
 
-	@property string command()
+	// UPDATE methods
+	ref QueryBuilder update(string table)
+	{
+		_table = table;
+		_type = QueryType.update;
+		return this;
+	}
+
+	ref QueryBuilder set(Value[string] params)
+	{
+		foreach (col, val; params)
+			set(col, val);
+
+		return this;
+	}
+
+	ref QueryBuilder set(T)(string col, T val)
+	{
+		_params[col] = val;
+		_set ~= "\"%s\" = {%s}".format(col, col);
+
+		return this;
+	}
+
+	ref QueryBuilder set(string set)
+	{
+		_set ~= set;
+
+		return this;
+	}
+
+	// Other stuff
+
+	private string replaceParams(string str)
+	{
+		int index = _paramIndex;
+
+		foreach (param, val; _params)
+			str = str.replace("{" ~ param ~ "}", "$%d".format(++index));
+
+		return str;
+	}
+
+	private string selectCommand()
 	{
 		string cols;
 		if (_columns.length == 0)
@@ -88,10 +162,10 @@ struct QueryBuilder
 		else
 			cols = "\"" ~ _columns.join("\", \"") ~ "\"";
 
-		string str = "SELECT %s FROM \"%s\"".format(cols, _from);
+		string str = "SELECT %s FROM \"%s\"".format(cols, _table);
 
-		if (_where.length > 0)
-			str ~= " WHERE " ~ _where;
+		if (_filter.length > 0)
+			str ~= " WHERE " ~ _filter;
 
 		if (_orderBy.length > 0)
 		{
@@ -107,28 +181,77 @@ struct QueryBuilder
 		if (!_offset.isNull)
 			str ~= " OFFSET %d".format(_offset);
 
-		foreach (param, val; _params)
-			str = str.replace("{" ~ param ~ "}", "$%d".format(_paramIndex++));
+		return replaceParams(str);
+	}
 
-		return str;
+	private string updateCommand()
+	{
+
+		string str = "UPDATE \"%s\" SET %s".format(
+				_table,
+				_set.join("\", \""));
+
+		if (_filter.length > 0)
+			str ~= " WHERE " ~ _filter;
+
+		return replaceParams(str);
+	}
+
+	@property string command()
+	{
+		final switch (_type)
+		{
+			case QueryType.select:
+				return selectCommand();
+			case QueryType.update:
+				return updateCommand();
+			case QueryType.insert:
+		}
+		return "err";
 	}
 
 	@property private Value[] paramsArr()
 	{
-		Value[] res;
+		Value[] res;// = _indexParams;
+		foreach (param; _indexParams)
+			res ~= param;
+
 		foreach (param, val; _params)
 			res ~= val;
 
-		std.stdio.writeln(res);
 		return res;
 	}
+
+
+	void addParam(T)(T val)
+	{
+		_indexParams ~= Value(val);
+		++_paramIndex;
+	}
+
+	ref QueryBuilder addParams(T...)(T vals)
+	{
+		foreach (val; vals)
+			addParam(val);
+
+		return this;
+	}
+
+	ref QueryBuilder opBinary(string op, T)(T val)
+			if (op == "<<")
+	{
+		addParam(val);
+
+		return this;
+	}
+
 
 	Query query()
 	{
 		if (_connection != null)
-			return Query(*_connection, command());
+			return Query(*_connection, command, paramsArr);
 
-		return Query(command());
+		return Query(command, paramsArr);
 	}
 
 	Query query(ref Connection conn)
