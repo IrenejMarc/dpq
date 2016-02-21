67e60d0e722910e88f8276e9c4a54be4b88315e2
Irenej Marc <irenej.marc@nuclei.co>
2016-02-21 15:16:15 +0100

    tests for column

--- source/dpq/column.d
+++ source/dpq/column.d
@@ -9,9 +9,23 @@ struct Column
 	{
 		return _asName.length > 0 ? _asName : column;
 	}
+
 	@property void asName(string n)
 	{
 		_asName = n;
 	}
 	//alias column this;
+
+	unittest
+	{
+		import std.stdio;
+		writeln(" * Column");
+		writeln("\t * asName");
+
+		Column c = Column("col", "col2");
+		assert(c.asName != c.column);
+
+		c = Column("col");
+		assert(c.asName == c.column);
+	}
 }
