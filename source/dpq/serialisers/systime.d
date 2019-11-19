3f9e6c717b46ab69ad654a37878f80de1b4e7861
Orfeo Da Vià <orfeo.davia@microline.eu>
2019-11-19 16:13:41 +0100

    Fix #33 dpq date time issue

--- source/dpq/serialisers/systime.d
+++ source/dpq/serialisers/systime.d
@@ -15,63 +15,62 @@ enum POSTGRES_EPOCH = DateTime(2000, 1, 1);
 
 struct SysTimeSerialiser
 {
-	static bool isSupportedType(T)()
-	{
-		return is(T == SysTime);
-	}
+   static bool isSupportedType(T)()
+   {
+      return is(T == SysTime);
+   }
 
-	static Nullable!(ubyte[]) serialise(T)(T val)
-	{
-		static assert (
-				isSupportedType!T,
-				"'%s' is not supported by SysTimeSerialiser".format(T.stringof));
+   static Nullable!(ubyte[]) serialise(T)(T val)
+   {
+      static assert (
+            isSupportedType!T,
+            "'%s' is not supported by SysTimeSerialiser".format(T.stringof));
 
-		alias RT = Nullable!(ubyte[]);
+      alias RT = Nullable!(ubyte[]);
 
-		if (isAnyNull(val))
-			return RT.init;
+      if (isAnyNull(val))
+         return RT.init;
 
-		// stdTime is in hnsecs, psql wants microsecs
-		long diff = val.stdTime - SysTime(POSTGRES_EPOCH).stdTime;
-		return RT(nativeToBigEndian(diff / 10).dup);
-	}
+      // stdTime is in hnsecs, psql wants microsecs
+      long diff = val.stdTime - SysTime(POSTGRES_EPOCH).stdTime;
+      return RT(nativeToBigEndian(diff / 10).dup);
+   }
 
-	static T deserialise(T)(const (ubyte)[] bytes)
-	{
-		static assert (
-				isSupportedType!T,
-				"'%s' is not supported by SysTimeSerialiser".format(T.stringof));
+   static T deserialise(T)(const (ubyte)[] bytes)
+   {
+      static assert (isSupportedType!T, "'%s' is not supported by SysTimeSerialiser".format(T.stringof));
 
-		return SysTime(fromBytes!long(bytes, long.sizeof) * 10 + SysTime(POSTGRES_EPOCH).stdTime);
-	}
+      import std.datetime.timezone : UTC;
+      return SysTime(fromBytes!long(bytes, long.sizeof) * 10 + SysTime(POSTGRES_EPOCH, UTC()).stdTime);
+   }
 
-	static Oid oidForType(T)()
-	{
-		return Type.TIMESTAMP;
-	}
+   static Oid oidForType(T)()
+   {
+      return Type.TIMESTAMP;
+   }
 
-	static string nameForType(T)()
-	{
-		return "TIMESTAMP";
-	}
+   static string nameForType(T)()
+   {
+      return "TIMESTAMP";
+   }
 
-	static void ensureExistence(T)(Connection c)
-	{
-		return;
-	}
+   static void ensureExistence(T)(Connection c)
+   {
+      return;
+   }
 }
 
 unittest
 {
-	import std.stdio;
-	import std.datetime;
+   import std.stdio;
+   import std.datetime;
 
-	writeln(" * SysTimeSerialiser");
+   writeln(" * SysTimeSerialiser");
 
-	// In reality, we should probably only check up to msec accuracy,
-	// and I'm not sure how much SysTimes == checks.
-	SysTime time = Clock.currTime;
-	auto serialised = SysTimeSerialiser.serialise(time);
+   // In reality, we should probably only check up to msec accuracy,
+   // and I'm not sure how much SysTimes == checks.
+   SysTime time = Clock.currTime;
+   auto serialised = SysTimeSerialiser.serialise(time);
 
-	assert(SysTimeSerialiser.deserialise!SysTime(serialised.get).toUnixTime == time.toUnixTime);
+   assert(SysTimeSerialiser.deserialise!SysTime(serialised.get).toUnixTime == time.toUnixTime);
 }
